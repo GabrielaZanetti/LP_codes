3a6d3b8a7b9f2f056afb93762a74d37cc70bb287
Gabriela da Rosa Zanetti <70355117+GabrielaZanetti@users.noreply.github.com>
2022-09-12 20:52:37 -0300

    unidade 3
    erro ao inserir tipos primitivos com um tamanho maior

---
+++ unidade-3/src/com/acme/eemplos/TipoPrimitivo.java
@@ -0,0 +1,14 @@
+package com.acme.eemplos;
+
+public class TipoPrimitivo {
+
+    public static void main(String[] args) {
+        int x = 127;
+        byte y = (byte) x;
+        
+//        int y = x (long);
+//        Erro: tamanho to tipo de variavel incompativel
+        
+        System.out.println(x);
+    }   
+}
