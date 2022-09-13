afeb5ea93b809e3be6cf87dc751f1c63106c95e5
Gabriela da Rosa Zanetti <70355117+GabrielaZanetti@users.noreply.github.com>
2022-09-12 22:12:35 -0300

    tipos primitivos
    tipos primitivos

--- unidade-3/src/com/acme/eemplos/TipoPrimitivo.java
+++ unidade-3/src/com/acme/eemplos/TipoPrimitivo.java
@@ -3,11 +3,23 @@ package com.acme.eemplos;
 public class TipoPrimitivo {
 
     public static void main(String[] args) {
-        char x = 'A';
+        int a = 40;
+        int b = a + a;
+//        System.out.println(b);
         
-//        int y = x (long);
-//        Erro: tamanho to tipo de variavel incompativel
+        char x = 'B';
+//        System.out.println(x);
         
-        System.out.println(x);
+        String s = "Rua das Flores";
+//        System.out.println(s);
+        
+//        char z = s.charAt(10); // posicao da string
+//        boolean z = s.contains("das"); // verifica se existe uma substring
+//        int z = s.length(); // soma da string
+//        String s2 = s.toString(); // soma da string
+        String s1 = "Ijui"; 
+        String s2 = "Ijui"; 
+        boolean v = s1.equalsIgnoreCase(s2); // comparacao de string
+        System.out.println(v);
     }   
 }
