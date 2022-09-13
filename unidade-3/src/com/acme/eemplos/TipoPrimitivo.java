4ec72bbaf3b70df9c002cd889782249085999dea
Gabriela da Rosa Zanetti <70355117+GabrielaZanetti@users.noreply.github.com>
2022-09-12 21:07:04 -0300

    tipo de inteiro com float

--- unidade-3/src/com/acme/eemplos/TipoPrimitivo.java
+++ unidade-3/src/com/acme/eemplos/TipoPrimitivo.java
@@ -3,8 +3,7 @@ package com.acme.eemplos;
 public class TipoPrimitivo {
 
     public static void main(String[] args) {
-        int x = 127;
-        byte y = (byte) x;
+        float x = 364.65f;
         
 //        int y = x (long);
 //        Erro: tamanho to tipo de variavel incompativel
