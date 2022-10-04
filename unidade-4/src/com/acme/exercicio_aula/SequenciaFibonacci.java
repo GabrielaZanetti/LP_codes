d2731fd9d3afa3687872a04f5bd205a591bdc259
Gabriela da Rosa Zanetti <70355117+GabrielaZanetti@users.noreply.github.com>
2022-10-03 22:15:17 -0300

    atualizacao
    mudanca do metodo de fibonacci

--- unidade-4/src/com/acme/exercicio_aula/SequenciaFibonacci.java
+++ unidade-4/src/com/acme/exercicio_aula/SequenciaFibonacci.java
@@ -7,10 +7,18 @@ package com.acme.exercicio_aula;
 public class SequenciaFibonacci {
     
     public static void main(String[] args) {
-        OperationFibiocci num = new OperationFibiocci();
-        int[] valor = num.generateNumbers();
-        for (int v : valor){
+        long[] valor = generateNumbers(10);
+        for (long v : valor){
             System.out.println(v);
         }
+    }
+    public static long[] generateNumbers(int x){
+        long[] number = new long[x];
+        number[0] = 0;
+        number[1] = 1;
+        for(int i = 2; i < x; i++){
+            number[i] = number[i-1] + number[i-2];
+        }
+        return number;
     }
 }
